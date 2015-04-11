05fb23d08174202808e284d0ca6bb9f39ed4318e
sonofthort <sonofthort@gmail.com>
2015-04-11 00:55:35 -0500

    Added implementation, explanation

--- README.md
+++ README.md
@@ -1,2 +1,35 @@
 # lambda
-Use symbol λ in JavaScript to write some very succient functions.
+Use symbol λ in JavaScript to write succient functions.
+# Explanation
+The first version of the function was this:
+~~~
+var λ = function(expr) {
+	return new Function('a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'return ' + expr)
+}
+~~~
+This allows writing functions like this:
+~~~
+var add = λ('a + b')
+add(8, 9) // returns 17
+~~~
+The Function constructor takes argument names until the last parameter, which is the function body. This causes the arguments to bind to instances of a, b, c, etc the argument to λ.
+
+After adding some more complex features, I was able to write functions like this:
+
+~~~
+var keysAndValues = λ([], [], λ.kv(a, A.push(k), B.push(v)), [A, B])
+~~~
+
+This is a function which returns a pair containing an array of all the keys and an array of all the values of an object. It also has placeholders applied to the local scope, which explain where the a, A, b, B, k, and v variables are coming from. Let's break this down:
+~~~
+A.push(k) // returns 'A.push(k)'
+~~~
+A is a placeholder, a function with toString() overloaded to return 'A', and the member 'push' returning 'A.push(...)' (... is whatever parameters are passed to it). Calling a placeholder has this effect:
+~~~
+A(a, b) // returns 'A(a, b)'
+~~~
+Each parameter to λ is a line in the resulting function, lines that do not begin with some control statement (for, if, return, etc) are treated as variable initializations. The first variable is named A for you, the second named B, and so on. Whenever you use 'A' or 'B' in your expression you are referring to the local variable. If the last line of the function does not begin with a control statement, it is returned.
+
+λ.kv is one of 6 looping functions. They loop over a collection (either an array or an object, or either in KV's case), expanding into inlined for-loops.
+
+[] and [A, B] are detected as arrays, and arrays are converted to strings such as '[]', '[A, B]'.
---
+++ lambda.js
@@ -0,0 +1,193 @@
+﻿// Created by Eric Thortsen, see LICENSE or https://github.com/sonofthort/lambda 
+
+lambda = {}
+
+lambda.lowercaseLetters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
+	'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+lambda.uppercaseLetters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
+	'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+
+lambda.letters = lambda.lowercaseLetters.concat(lambda.uppercaseLetters)
+
+lambda.DEBUG = false
+
+λ = function() {
+	var lines = []
+	try {
+		function addLines(src, header) {
+			var length = src.length,
+				last = src.length - 1
+			for (var i = 0; i < length; ++i) {
+				var line = src[i]
+				if (line instanceof Array) {
+					line = '[' + line.join(', ') + ']'
+				} else if (typeof line === 'object') {
+					var values = []
+					for (var k in line) {
+						if (line.hasOwnProperty(k)) {
+							values.push(k + ' = ' + line[k])
+						}
+					}
+					line = '{' + values.join(', ') + '}'
+				}
+				line = line.toString()
+				switch (line.substring(0, line.search(/[\s\(]/))) {
+					case 'if':
+					case 'do':
+					case 'let':
+					case 'for':
+					case 'var':
+					case 'with':
+					case 'while': break
+					default: line = (i < last ? 'var ' + lambda.uppercaseLetters[i] + ' = ' : 'return ') + line
+				}
+				lines.push(header + line)
+			}
+		}
+		if (lambda.DEBUG) {
+			lines.push('try {')
+			addLines(arguments, '\t')
+			lines.push('} catch(e) {')
+			lines.push('\talert("func error: " + e + ", stack: " + (new Error()).stack)')
+			lines.push('}')
+		} else {
+			addLines(arguments, '')
+		}
+		var joined = lines.join('\n'),
+			result = new Function('a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', joined)
+		//result.toString = 'function(a, b, c, d, e, f, g, h) {' + lines + '}'
+		return result
+	} catch(e) {
+		alert(e + ':\n' + lines.join('\n') + '\nstack:\n\n' + (new Error()).stack)
+	}
+}
+
+Λ = function(comments) {
+	return λ
+}
+
+λ.k = function(o) {
+	var k = 'k'
+	if (o instanceof Array) {
+		k = o[1] || k
+		o = o[0]
+	}
+	var exprs = Array.prototype.slice.call(arguments, 1).join('; ')
+	return ['for (var ', k, ' in ', o, ') if (', o, '.hasOwnProperty(', k, ')) {', exprs, '}'].join('')
+}
+
+λ.kv = function(o) {
+	var k = 'k',
+		v = 'v'
+	if (o instanceof Array) {
+		v = o[2] || v
+		k = o[1] || k
+		o = o[0]
+	}
+	var args = Array.prototype.slice.call(arguments, 1)
+	args.unshift([o, k], 'var ' + v + ' = ' + o + '[' + k + ']')
+	return λ.k.apply(null, args)
+}
+
+λ.i = function(a) {
+	var i = 'i'
+	if (a instanceof Array) {
+		i = a[1] || i
+		a = a[0]
+	}
+	var exprs = Array.prototype.slice.call(arguments, 1).join('; ')
+	return ['for (var ', i, ' = 0, l = ', a, '.length; ', i, ' < l; ++', i, ') {', exprs, '}'].join('')
+}
+
+λ.iv = function(a) {
+	var i = 'i',
+		v = 'v'
+	if (a instanceof Array) {
+		i = a[1] || i
+		a = a[0]
+	}
+	var args = Array.prototype.slice.call(arguments, 1)
+	args.unshift([a, i], 'var ' + v + ' = ' + a + '[' + i + ']')
+	return λ.i.apply(null, args)
+}
+
+λ.fi = function(expr, onTrue, onFalse) {
+	return 'if (' + expr + ') {' + onTrue + '}' + (onFalse ? (' else {' + onFalse + '}') : '')
+}
+
+λ.K = function(o) {
+	var k = 'k'
+	if (o instanceof Array) {
+		k = o[1] || k
+		o = o[0]
+	}
+	var args = Array.prototype.slice.call(arguments, 0)
+	args[0] = [o, k]
+	return λ.fi(o + ' instanceof Array', λ.i.apply(null, args), λ.k.apply(null, args))
+}
+
+λ.KV = function(o) {
+	var k = 'k',
+		v = 'v'
+	if (o instanceof Array) {
+		v = o[2] || v
+		k = o[1] || k
+		o = o[0]
+	}
+	var args = Array.prototype.slice.call(arguments, 0)
+	args[0] = [o, k, v]
+	return λ.fi(o + ' instanceof Array', λ.iv.apply(null, args), λ.kv.apply(null, args))
+}
+
+λ.r = function(expr) {
+	return 'return ' + expr
+}
+
+λ.args = function(begin) {
+	return 'Array.prototype.slice.call(arguments, ' + (begin ? begin : 0) + ')'
+}
+
+lambda.each = λ(λ.KV('a', 'b(v, k)'), 'a')
+
+λ.placeholders = {}
+
+λ.placeholder = function(value) {
+	var result = function() {
+		return value + '(' + Array.prototype.slice.call(arguments).join(', ') + ')'
+	}
+	result.at = function(key) {
+		return value + '[' + key + ']'
+	}
+	result.set = function(key, val) {
+		return value + '[' + key + '] = ' + val
+	}
+	result.toString = function() {return value}
+	result.addPlaceholder = function(name) {
+		return result[name] = λ.placeholder(value + '.' + name)
+	}
+	return result
+}
+
+λ.addPlaceholder = function(name, value) {
+	var result = λ.placeholders[name] = λ.placeholder(value)
+	;['apply', 'call', 'push', 'map'].forEach(result.addPlaceholder)
+	return result
+}
+
+;[	['nil', 'null'],
+	['that', 'this'],
+	['args', 'arguments']].
+	concat(lambda.letters.map(λ('[a, a]'))).
+	forEach(λ('λ.addPlaceholder(a[0], a[1])'))
+
+λ.localPlaceholders = function(header) {
+	header = header || ''
+	var vars = []
+	lambda.each(λ.placeholders, function(v, k) {
+		vars.push(header + k + ' = λ.placeholders.' + k)
+	})
+	return 'var ' + vars.join(', ')
+}
+
+// eval(λ.localPlaceholders('λ'))
